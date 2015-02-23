99f82961f18faef80e52e4ac9affb2b25db519c3
Bob Moore <bobjutsu@gmail.com>
2015-02-23 16:08:39 -0600

    gitignore

--- App/ajax/ajax_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbuser   = "root";
-    $dbpass   = "1234";
-    $dsn      = "mysql:host=127.0.0.1;dbname=hollyhotel";
-    $db = new PDO($dsn, $dbuser, $dbpass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
