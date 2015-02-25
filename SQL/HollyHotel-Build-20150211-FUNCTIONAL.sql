681a998af9f447a043dede3052bfd631fa74f790
pharsh <pharsh@users.noreply.github.com>
2015-02-25 10:06:21 -0600

    cc_info
    Missed the Credit Card Network originall (visa/discover, etc.)

--- SQL/HollyHotel-Build-20150211-FUNCTIONAL.sql
+++ SQL/HollyHotel-Build-20150211-FUNCTIONAL.sql
@@ -686,11 +686,12 @@ COMMENT = 'Simple table to implement basic authentication';
 
 DROP TABLE IF EXISTS `CC_INFO`;
 
-CREATE TABLE IF NOT EXISTS `CC_INFO` (
+CREATE TABLE IF NOT EXISTS `hollyhotel`.`CC_INFO` (
   `CCID` INT NOT NULL AUTO_INCREMENT COMMENT 'Surrogate Key',
   `CustomerID` INT NOT NULL COMMENT 'Links Customer to the Credit Card',
   `AddressSeq` INT NOT NULL COMMENT 'Links Credit Card to billing address.  If the same as the primary then 0, otherwise add new sequence',
   `NameOnCard` VARCHAR(100) NULL,
+  `CreditCardNetwork` VARCHAR(45) NULL COMMENT 'Visa, Mastercard, Discover',
   `CCNumber` VARCHAR(16) NOT NULL COMMENT 'Credit card Number, Normally12 digits',
   `ExpMonth` INT NULL COMMENT 'Expiration Month 1-12',
   `ExpYear` INT NULL COMMENT 'Expiration Year',
