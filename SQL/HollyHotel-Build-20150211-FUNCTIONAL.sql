4b6d00b3ae30731f4da710bdf32e9e45cd64bef5
pharsh <pharsh@users.noreply.github.com>
2015-02-26 08:26:35 -0600

    staycharges

--- SQL/HollyHotel-Build-20150211-FUNCTIONAL.sql
+++ SQL/HollyHotel-Build-20150211-FUNCTIONAL.sql
@@ -1118,27 +1118,32 @@ group by s.StayID;$$
 DROP VIEW IF EXISTS `StayChargesVw` $$
 
 CREATE VIEW `StayChargesVw` AS
-SELECT 
+select 
 sc.StayID
-, s.RoomID
-, rm.RoomNumber
-, sc.ChargeTo
-, c.FirstName 
+, r.RoomID
+, r.RoomNumber
+, c.Firstname
 , c.LastName
-, sc.ChargeType
-, ct.Name as ChargeDesc
+, c.BillToAddress1
+, c.BillToAddress2
+, c.BillToAddress3
+, c.BillToAddress4
+, c.BillToCity
+, c.BillToState
+, c.BillToZip
+, c.BillToCountry
+, c.BillToPhoneNum
+, sc.ChargeType as ChargeCodeID
+, ct.Name as ChargeCode
 , sc.ChargeDescription
-, sc.Amount
 , sc.ChargeDate
 , sc.DueDate
 , sc.PaidDate
-
-from STAY_CHARGES sc
-join TYPE_NAME ct on ct.TypeNameID=sc.ChargeType
-join STAY s on s.StayID-sc.StayID
-join ROOM rm on rm.RoomID=s.RoomID
-JOIN CUSTOMER c on c.CustomerID=sc.ChargeTo
-
+from stay_charges sc
+join stay s on s.StayID=sc.StayID
+join stdcustomerinfovw c on c.customerid=sc.ChargeTo
+join room r on r.roomid=s.RoomID
+join type_name ct on ct.TypeNameID=sc.ChargeType
 WHERE IFNULL(s.Checkout,NOW()) >= DATE_ADD(NOW(), INTERVAL -14 DAY) 
 #COMMENT 'USED TO SUMMARIZE CHARGES WITHOUT CREATING JOINS IN THE UI'
 ;$$
